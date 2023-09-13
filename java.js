e7d2007915f9220473908a2a8dad1b3bc374bb6d
manuel <manuher1229@hotmail.com>
2023-09-13 05:39:02 -0400

    Last funtions added
    The last two functiones were added and the game is now working, the best of five wins

--- java.js
+++ java.js
@@ -12,39 +12,71 @@ function getComputerChoice() {
     }
 }
 
-function round(playerSelection, ComputerSelection) {
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase()
     if (playerSelection == "rock") {
-        if (ComputerSelection == "rock") {
+        if (computerSelection == "rock") {
             return "Its a draw! Nobody wins this round."
         }
-        else if (ComputerSelection == "paper") {
+        else if (computerSelection == "paper") {
+            scoreIa += 1;
+
             return "You lose! Paper beats Rock."
         }
         else {
+            scoreUser += 1;
             return "You win! Rock beats Scissors."
         }
     }
     else if (playerSelection == "paper") {
-        if (ComputerSelection == "rock") {
+        if (computerSelection == "rock") {
+            scoreUser += 1;
             return "You win! Paper beats Rock."
         }
-        else if (ComputerSelection == "paper") {
+        else if (computerSelection == "paper") {
             return "Its a draw! Nobody wins this round."
         }
         else {
+            scoreIa += 1;
+
             return "You lose! Scissors beats Paper."
         }
     }
     else {
-        if (ComputerSelection == "rock") {
+        if (computerSelection == "rock") {
+            scoreIa += 1;
+
             return "You lose! Rock beats Scissors."
         }
-        else if (ComputerSelection == "paper") {
-            return "You Win! Scissors beats Paper."
+        else if (computerSelection == "paper") {
+            scoreUser += 1;
+            return "You win! Scissors beats Paper."
         }
         else {
             return "Its a draw! Nobody wins this round."
         }
     }
 }
-console.log(round("paper", getComputerChoice()))
+
+let scoreIa = 0;
+let scoreUser = 0;
+
+function game() {
+
+
+    while (scoreIa < 5 && scoreUser < 5) {
+        const playerSelection = prompt("Make your selection(Rock, Paper or Scissor");
+        const computerSelection = getComputerChoice();
+
+        let play = (playRound(playerSelection, computerSelection));   
+        console.log(play, scoreIa, scoreUser)  
+    }
+    if (scoreIa == 5) {
+        return "The computer wins!"
+    }
+    else {
+        return "The user wins!"
+    }
+}
+
+console.log(game())
